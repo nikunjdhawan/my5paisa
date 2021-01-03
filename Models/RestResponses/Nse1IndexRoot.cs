2336d6be365cb63739b9d7455bf844bbcc839a52
Nikunj Dhawan <nikunjdhawan@gmail.com>
2021-01-03 19:56:06 +0530

    ns1index

---
+++ Models/RestResponses/Nse1IndexRoot.cs
@@ -0,0 +1,60 @@
+
+using System.Collections.Generic;
+
+namespace My5Paisa.Models
+{    
+    public class Nse1IndexDatum    
+    {
+        public string symbol { get; set; } 
+        public double open { get; set; } 
+        public double high { get; set; } 
+        public double low { get; set; } 
+        public double ltP { get; set; } 
+        public double ptsC { get; set; } 
+        public double per { get; set; } 
+        public double trdVol { get; set; } 
+        public double trdVolM { get; set; } 
+        public double ntP { get; set; } 
+        public double mVal { get; set; } 
+        public double wkhi { get; set; } 
+        public double wklo { get; set; } 
+        public double wkhicm_adj { get; set; } 
+        public double wklocm_adj { get; set; } 
+        public string xDt { get; set; } 
+        public string cAct { get; set; } 
+        public double previousClose { get; set; } 
+        public double dayEndClose { get; set; } 
+        public double iislPtsChange { get; set; } 
+        public double iislPercChange { get; set; } 
+        public double yPC { get; set; } 
+        public double mPC { get; set; } 
+    }
+
+    public class Nse1IndexLatestData    {
+        public string indexName { get; set; } 
+        public string open { get; set; } 
+        public string high { get; set; } 
+        public string low { get; set; } 
+        public string ltp { get; set; } 
+        public string ch { get; set; } 
+        public string per { get; set; } 
+        public string yCls { get; set; } 
+        public string mCls { get; set; } 
+        public string yHigh { get; set; } 
+        public string yLow { get; set; } 
+    }
+
+    public class Nse1IndexRoot    {
+        public double declines { get; set; } 
+        public List<Nse1IndexDatum> data { get; set; } 
+        public string trdVolumesum { get; set; } 
+        public List<Nse1IndexLatestData> latestData { get; set; } 
+        public double advances { get; set; } 
+        public int unchanged { get; set; } 
+        public string trdValueSumMil { get; set; } 
+        public string time { get; set; } 
+        public string trdVolumesumMil { get; set; } 
+        public string trdValueSum { get; set; } 
+    }
+
+}
--- Models/Strategies/PreviousDayHighLowOpen.cs
+++ Models/Strategies/PreviousDayHighLowOpen.cs
@@ -52,29 +52,38 @@ namespace My5Paisa.Models
             // var httpClient = new HttpClient();
             // string resp = httpClient.GetStringAsync("https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%2050").Result;
             if (trades.Count > 0) return;
-            var client = new RestClient("https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%2050");
+            var client = new RestClient("https://www1.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66";
             request.AddHeader("accept-language", "en-US,en;q=0.9");
+            request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            request.AddHeader("sec-fetch-dest", "document");
+            request.AddHeader("sec-fetch-mode", "navigate");
+            request.AddHeader("sec-fetch-site", "none");
+            request.AddHeader("upgrade-insecure-requests", "1");
             request.AddHeader("Cookie", "");
             IRestResponse response = client.Execute(request);
-
-
-            NseIndexRoot nifty50 = JsonConvert.DeserializeObject(response.Content, typeof(NseIndexRoot)) as NseIndexRoot;
-
-
-
-            int buyOrdersCount = (int)(nifty50.advance.advances / (nifty50.advance.advances + nifty50.advance.declines) * 10);
-
-            foreach (var item in nifty50.data.Where(i => i.open == i.dayLow && i.lastPrice < 5000).Take(buyOrdersCount))
+            while(response.Content.StartsWith("{") == false)
             {
-                TradeCall tc = new TradeCall { ScriptName = item.symbol, Price = item.dayHigh, OrderType = "Buy" };
+                response = client.Execute(request);
+            }
+
+
+            Nse1IndexRoot nifty50 = JsonConvert.DeserializeObject(response.Content, typeof(Nse1IndexRoot)) as Nse1IndexRoot;
+
+
+
+            int buyOrdersCount = (int)(nifty50.advances / (nifty50.advances + nifty50.declines) * 10);
+
+            foreach (var item in nifty50.data.Where(i => i.open == i.low && i.ltP < 5000).Take(buyOrdersCount))
+            {
+                TradeCall tc = new TradeCall { ScriptName = item.symbol, Price = item.high, OrderType = "Buy" };
                 trades.Add(tc);
             }
-            foreach (var item in nifty50.data.Where(i => i.open == i.dayHigh && i.lastPrice < 5000).Take(10 - buyOrdersCount))
+            foreach (var item in nifty50.data.Where(i => i.open == i.high && i.ltP < 5000).Take(10 - buyOrdersCount))
             {
-                TradeCall tc = new TradeCall { ScriptName = item.symbol, Price = item.dayLow, OrderType = "Sell" };
+                TradeCall tc = new TradeCall { ScriptName = item.symbol, Price = item.low, OrderType = "Sell" };
                 trades.Add(tc);
             }
         }
